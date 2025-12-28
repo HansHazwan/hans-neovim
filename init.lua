12b28ef129ee6feb97b3259a880187092184379c
Hans Landa <muhammadhazwanzufar79@gmail.com>
2025-12-28 23:38:53 +0800

    Fix code in formatter.lua

--- init.lua
+++ init.lua
@@ -2,4 +2,3 @@ require("hans.core.options")
 require("hans.core.keymaps")
 require("hans.core.autocmd")
 require("hans.lazy")
-
--- lua/hans/plugins/formatter.lua
+++ lua/hans/plugins/formatter.lua
@@ -1,21 +1,22 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  opts = {
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
-    formatter_by_ft = {
-      lua = { "stylua" },
-      javascript = { "prettier" },
-      typescript = { "prettier" },
-      json = { "prettier" },
-      html = { "prettier" },
-      css = { "prettier" },
-      python = { "black" },
-      go = { "gofmt" },
-      rust = { "rustfmt" },
-    }
-  }
+	"stevearc/conform.nvim",
+	config = function()
+		require("conform").setup({
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				json = { "prettier" },
+				html = { "prettier" },
+				css = { "prettier" },
+				python = { "black" },
+				go = { "gofmt" },
+				rust = { "rustfmt" },
+			},
+		})
+	end,
 }
