429d417d776c024f5584bc5fd4c70320f64164c7
Hans Landa <muhammadhazwanzufar79@gmail.com>
2025-12-29 00:03:47 +0800

    Fix some error in system by download luarocks for install luacheck

---
+++ lua/hans/plugins/lint.lua
@@ -0,0 +1,23 @@
+return {
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPost", "BufNewFile" },
+	config = function()
+		local lint = require("lint")
+
+		lint.linters_by_ft = {
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			html = { "htmlhint" },
+			css = { "stylelint" },
+			lua = { "luacheck" },
+			python = { "flake8" },
+			sh = { "shellcheck" },
+		}
+
+		vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "InsertLeave" }, {
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+	end,
+}
