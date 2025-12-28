d1b05410d073f7d6ec1c990bed19d126f2cac06f
Hans Landa <muhammadhazwanzufar79@gmail.com>
2025-12-29 00:40:13 +0800

    Adding react.js configuration lsp

--- lua/hans/lsp/lspconfig.lua
+++ lua/hans/lsp/lspconfig.lua
@@ -1,20 +1,31 @@
-vim.lsp.config['lua_ls'] = {
-    cmd = { 'lua-language-server' },
-    filetypes = { 'lua' },
-    root_markers = {
-      '.luarc.json',
-      '.luarc.jsonc',
-      '.luacheckrc',
-      '.stylua.toml',
-      'stylua.toml',
-      'selene.toml',
-      'selene.yml',
-      '.git',
-    }
+vim.lsp.config["lua_ls"] = {
+	cmd = { "lua-language-server" },
+	filetypes = { "lua" },
+	root_markers = {
+		".luarc.json",
+		".luarc.jsonc",
+		".luacheckrc",
+		".stylua.toml",
+		"stylua.toml",
+		"selene.toml",
+		"selene.yml",
+		".git",
+	},
 }
 
 vim.lsp.enable("lua_ls")
 
+vim.lsp.config["ts_ls"] = {
+	cmd = { "typescript-language-server", "--stdio" },
+	filetypes = {
+		"javascript",
+		"javascriptreact",
+		"typescript",
+		"typescriptreact",
+	},
+}
+
+vim.lsp.enable("ts_ls")
 
 vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
